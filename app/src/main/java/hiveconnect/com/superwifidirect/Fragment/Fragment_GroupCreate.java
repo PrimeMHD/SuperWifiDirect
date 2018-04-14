9efdb2ab34542ba872fdc1023fc484994b44e081
Mi <mhd@tongji.edu.cn>
2018-04-14 17:22:29 +0800

    引入了EventBus

--- app/src/main/java/hiveconnect/com/superwifidirect/Fragment/Fragment_GroupCreate.java
+++ app/src/main/java/hiveconnect/com/superwifidirect/Fragment/Fragment_GroupCreate.java
@@ -5,11 +5,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pGroup;
 import android.net.wifi.p2p.WifiP2pInfo;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,23 +20,35 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
 import java.util.Collection;
 
 import hiveconnect.com.superwifidirect.Activity.MainActivity;
+import hiveconnect.com.superwifidirect.Adapter.DeviceAdapter;
+import hiveconnect.com.superwifidirect.Bean.Event_FunctionFragmentEvent;
 import hiveconnect.com.superwifidirect.Callback.DirectActionListener;
 import hiveconnect.com.superwifidirect.Fragment.BasicFragment.MySupportFragment;
 import hiveconnect.com.superwifidirect.R;
 import hiveconnect.com.superwifidirect.Service.WifiServerService;
+import hiveconnect.com.superwifidirect.util.EnumPack;
 
 //import static android.content.ContentValues.TAG;
 
-public class Fragment_GroupCreate extends MySupportFragment implements DirectActionListener {
+public class Fragment_GroupCreate extends MySupportFragment{
 
 
     private Button button_CreateGroup;
     private Button button_DismissGroup;
+    private Button button_ChooseFunc_Trans;
+    private Button button_ChooseFunc_Sign;
+    private Button button_ChooseFunc_SeekHelp;
+    private RecyclerView rv_SlaveList;
+    private DeviceAdapter deviceAdapter;
     private static final String TAG="Fragment_GroupCreate";
-
+    private static EnumPack.GroupFunc groupFunc= EnumPack.GroupFunc.NOFUNC;
 
 
 
@@ -44,18 +59,49 @@ public class Fragment_GroupCreate extends MySupportFragment implements DirectAct
         fragment.setArguments(args);
         return fragment;
     }
-    @Nullable
+
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onMessageEvent(Event_FunctionFragmentEvent event) {
+        Log.e(TAG,"收到了EventBus信息"+event.getmConcreteEvent());
+        switch (event.getmConcreteEvent()){
+            case wifiP2pEnabled:
+                handleEvent_wifiP2pEnabled();;
+                break;
+            case onDisconnection:
+                handleEvent_onDisconnection();
+                break;
+            case onPeersAvailable:
+                handleEvent_onPeersAvailable();
+                break;
+            case onSelfDeviceAvailable:
+                handleEvent_onSelfDeviceAvailable();
+                break;
+            case onConnectionInfoAvailable:
+                handleEvent_onConnectionInfoAvailable();
+                break;
+                default:
+                    break;
+        }
+    };
+
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         fragmentView = inflater.inflate(R.layout.layout_fragment_formgroup_master, container, false);
-        button_CreateGroup=(Button)fragmentView.findViewById(R.id.button_CreateGroup);
-        button_DismissGroup=(Button)fragmentView.findViewById(R.id.button_DismissGroup);
-        setTouchEvent();
+        EventBus.getDefault().register(this);
+        deviceAdapter=new DeviceAdapter(mainActivity.wifiP2pSlaveList);
+        initView();
         return fragmentView;
     }
 
-
-    private void setTouchEvent(){
+    private void initView() {
+        button_CreateGroup=(Button)fragmentView.findViewById(R.id.button_CreateGroup);
+        button_DismissGroup=(Button)fragmentView.findViewById(R.id.button_DismissGroup);
+        button_ChooseFunc_Trans=(Button)fragmentView.findViewById(R.id.button_ChooseFunc_Trans);
+        rv_SlaveList=(RecyclerView)fragmentView.findViewById(R.id.rv_SlaveList);
         button_CreateGroup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -66,8 +112,49 @@ public class Fragment_GroupCreate extends MySupportFragment implements DirectAct
             @Override
             public void onClick(View view) {
                 removeGroup();
+                //TODO 清空设备List
+
             }
         });
+        button_ChooseFunc_Trans.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                groupFunc= EnumPack.GroupFunc.TRANS;
+
+            }
+        });
+        deviceAdapter = new DeviceAdapter(mainActivity.wifiP2pSlaveList);
+        deviceAdapter.setClickListener(new DeviceAdapter.OnClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                //TODO 可以在这里设置驱逐的方法
+            }
+        });
+        rv_SlaveList.setAdapter(deviceAdapter);
+        rv_SlaveList.setLayoutManager(new LinearLayoutManager(mainActivity));
+
+
+    }
+
+    private void handleEvent_wifiP2pEnabled(){
+
+    }
+
+    private void handleEvent_onConnectionInfoAvailable(){
+        deviceAdapter.notifyDataSetChanged();
+
+
+    }
+
+    private void handleEvent_onDisconnection(){
+
+    }
+
+    private void handleEvent_onSelfDeviceAvailable(){
+
+    }
+
+    private void handleEvent_onPeersAvailable(){
 
     }
 
@@ -84,6 +171,14 @@ public class Fragment_GroupCreate extends MySupportFragment implements DirectAct
                 Log.e(TAG, "createGroup onSuccess");
                 //dismissLoadingDialog();
                 showToast("onSuccess");
+                wifiP2pManager.requestGroupInfo(channel, new WifiP2pManager.GroupInfoListener() {
+                    @Override
+                    public void onGroupInfoAvailable(WifiP2pGroup wifiP2pGroup) {
+
+                        Log.e(TAG,"获取到了wifiP2pGroup的信息");
+                        Log.e(TAG,wifiP2pGroup+"");
+                    }
+                });
             }
 
             @Override
@@ -112,48 +207,9 @@ public class Fragment_GroupCreate extends MySupportFragment implements DirectAct
         });
     }
 
-
-
-
-
-    //implements methods of DirectActionListener
-
     @Override
-    public void wifiP2pEnabled(boolean enabled) {
-        Log.e(TAG, "wifiP2pEnabled: " + enabled);
+    public void onDetach() {
+        super.onDetach();
+        EventBus.getDefault().unregister(this);
     }
-
-    @Override
-    public void onConnectionInfoAvailable(WifiP2pInfo wifiP2pInfo) {
-        Log.e(TAG, "onConnectionInfoAvailable");
-        Log.e(TAG, "isGroupOwner：" + wifiP2pInfo.isGroupOwner);
-        Log.e(TAG, "groupFormed：" + wifiP2pInfo.groupFormed);
-        if (wifiP2pInfo.groupFormed && wifiP2pInfo.isGroupOwner) {
-            if (wifiServerService != null) {
-                mainActivity.startWifiServerSerivce();
-            }
-        }
-    }
-
-    @Override
-    public void onDisconnection() {
-        Log.e(TAG, "onDisconnection");
-    }
-
-    @Override
-    public void onSelfDeviceAvailable(WifiP2pDevice wifiP2pDevice) {
-        Log.e(TAG, "onSelfDeviceAvailable");
-    }
-
-    @Override
-    public void onPeersAvailable(Collection<WifiP2pDevice> wifiP2pDeviceList) {
-        Log.e(TAG, "onPeersAvailable");
-    }
-
-    @Override
-    public void onChannelDisconnected() {
-        Log.e(TAG, "onChannelDisconnected");
-    }
-
-
 }
